1cc91571d0512caac31366bc29fde869d571684d
Miguel <jmiguel.rnavarro@gmail.com>
2024-05-02 22:18:08 +0200

    added tailwind

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,22 +1,17 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+import Chat from './components/Chat';
+
 
 function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      
-    </>
+    <div className='bg-red-600 h-full'>
+      <Chat></Chat>
+    </div>
   )
 }
 
---
+++ webview-ui/src/components/Chat.tsx
@@ -0,0 +1,20 @@
+import Grid from '@mui/material/Unstable_Grid2';
+
+export default function Chat() {
+    return (
+        <Grid container direction="column" justifyContent="space-between" alignItems="stretch">
+            <Grid xs={12}>
+                xs=12
+            </Grid>
+            <Grid xs={12}>
+                xs=12
+            </Grid>
+            <Grid xs={12}>
+                xs=12
+            </Grid>
+            <Grid xs={12}>
+                xs=12
+            </Grid>
+        </Grid>
+    )
+}
